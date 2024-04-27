b0c11a07a5246daf02aca96e30b6805d120f7df7
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-04-27 17:45:26 +0300

    new

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { IgxDatePickerModule, IgxTimePickerModule } from 'igniteui-angular';
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, IgxDatePickerModule, IgxTimePickerModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  public date = new Date();
 }
