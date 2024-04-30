929056e70dfda88af79f997c097e6da4eed3941c
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-04-30 21:14:24 +0300

    new

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IgxDatePickerModule, IgxTimePickerModule } from 'igniteui-angular';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, IgxDatePickerModule, IgxTimePickerModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  public date = new Date();
 }
--- src/app/components/portfolio/case-study/case-study.component.ts
+++ src/app/components/portfolio/case-study/case-study.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { IgxDatePickerModule, IgxTimePickerModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-case-study',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, IgxDatePickerModule, IgxTimePickerModule],
   templateUrl: './case-study.component.html',
-  styleUrl: './case-study.component.scss'
+  styleUrl: './case-study.component.scss',
 })
 export class CaseStudyComponent {
-
+  public date = new Date();
 }
